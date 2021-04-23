6721b2234211d3971383a2e07960b693c0e118d6
Brendon dos Reis Carvalho <contato@brendon.com.br>
2021-04-22 22:29:05 -0300

    Fontes, icone e ajustes no layout da tela Welcome.

---
+++ src/pages/UserIdentification.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+
+export function UserIdentification(){
+    return(
+        <SafeAreaView style={styles.container}>
+
+        </SafeAreaView>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
